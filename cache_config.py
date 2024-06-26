d1707aa9d9af8ebf8f86a3ed71c2ab0ecf82b4dc
Victor Mulyungi <mulyungivick09@gmail.com>
2024-06-26 15:29:18 +0300

    reset the config file

--- cache_config.py
+++ cache_config.py
@@ -1,10 +1,9 @@
-#redis://redis-15624.c15.us-east-1-2.ec2.redns.redis-cloud.com:15624
 
 import os
 from flask_caching import Cache
 
 CACHE_CONFIG={
     'CACHE_TYPE':'RedisCache',
-    'CACHE_REDIS_URL':os.environ.get('REDIS_URL', 'redis://localhost:6379')
+    'CACHE_REDIS_URL':os.environ.get('REDIS_URL', 'redis://redis-15624.c15.us-east-1-2.ec2.redns.redis-cloud.com:15624')
 }
 cache=Cache(config=CACHE_CONFIG)
